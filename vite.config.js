cfcc5d167f2943403fb8ca4c86464bea6d2d0ddc
Evandro Silva <evandro.silva@exemplo.com>
2026-06-01 14:35:37 -0300

    chore: remove ssl plugin to fix production deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [
-    react(),
-    command === 'serve' ? basicSsl() : null
-  ],
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: true,
     watch: {
       usePolling: true,
     },
   },
-}))
+})
